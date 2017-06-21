62e73fe86eeacf721373ede8ce3a2f97f572e343
joe <joemulick@gmail.com>
2017-06-21 11:57:06 -0700

    Updated with elmas work- Joe

--- app/components/Grandchild.js
+++ app/components/Grandchild.js
@@ -8,28 +8,33 @@ var GrandChild = React.createClass({
     console.log(this.props.data);
     return (
     <div className="row grandchild">
-      <div className="col-sm-12 display-box">
-        
-        <div className="panel panel-default ">
-          <div className="panel-heading">
-            <h3 className="panel-title">this is where the legislation is posted</h3>
-            </div>
-            <div className="render-container panel-body text-center">
-            <h3> HB 2017 </h3>
-            <h5> here we have a description of what the legislation will be, taken from the 'title' of the api </h5>
-              {/*
-                This component will display 4 times what it's parent (Child) displays
-
-                          
-
-              */}
-              <h1 className="render-h1">{this.props.data.map(function(arg){
-                return (<h2>{arg.title}</h2>);
-              })}</h1>
-            <button> button </button>
-            </div>
-          </div>
+    <div className="col-sm-12 display-box">
+      
+      <div className="panel panel-default ">
+        <div className="panel-heading">
+          <h3 className="panel-title">this is where the legislation is posted</h3>
         </div>
+        <div className="panel-body">
+        <h3> HB 2017 </h3>
+        <h5> here we have a description of what the legislation will be, taken from the 'title' of the api </h5>
+          {/*
+            This component will display 4 times what it's parent (Child) displays
+          */}
+          <div className="grandchild-title-div">
+            <ol>
+            {
+              this.props.data.map(function(arg){
+               return (<li className="grandchild-title">
+                        <a href="https://openstates.org/ca/bills/{arg.session}/{arg.bill_id}/" target="_blank">{arg.title}</a>
+                        </li>);
+              })
+            }
+            </ol>
+          </div>
+         
+        </div>
+      </div>
+      </div>
       </div>
     );
   }
