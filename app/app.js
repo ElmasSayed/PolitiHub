a32bfcd759c533accab3d9ce6523de52c30c3288
ElmasSayed <elmassayed@gmail.com>
2017-06-22 11:02:21 -0700

    thursday morning

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 // Include the Main React Dependencies
 var React = require("react");
 var ReactDOM = require("react-dom");
+
+// Include the main Parent Component
 var Parent = require("./components/Parent");
 
 // This code here allows us to render our main component (in this case Parent)
--- app/components/Grandchild.js
+++ app/components/Grandchild.js
@@ -1,41 +1,43 @@
 // Include React
 var React = require("react");
 
+// the grandchild is the actual posting of all the articles
 var GrandChild = React.createClass({
- 
+   
+getInitialState: function(){
+  return { text : this.props.text };
+},
   render: function() {
-
+    console.log(this.props.data);
     return (
     <div className="row grandchild">
     <div className="col-sm-12 display-box">
       
       <div className="panel panel-default ">
         <div className="panel-heading">
-          <h3 className="panel-title text-center">Most Recent Bills</h3>
+          <h3 className="panel-title">this is where the legislation is posted</h3>
         </div>
         <div className="panel-body">
         <h3> HB 2017 </h3>
         <h5> here we have a description of what the legislation will be, taken from the 'title' of the api </h5>
-
+          {/*
+            This component will display 4 times what it's parent (Child) displays
+          */}
           <div className="grandchild-title-div">
-            <ol>
-
-              {
-                this.props.data.map((arg) => {
-                  if (this.props.categories == "House" || "Senate"){
-                   return (
-                      <li className="grandchild-title">
-                      <a href={arg.govtrack_url} target="_blank">{arg.title}</a>
-                      </li>);
-                  } else {
-                      return (
-                      <li className="grandchild-title">
-                      <a href="https://openstates.org/ca/bills/{arg.session}/{arg.bill_id}/" target="_blank">{arg.title}</a>
-                      </li>);
-                  }
-                })
+          {this.props.text}
+          {
+                
+               this.props.data.map(function(bill, index){
+               return (<div key={index} className="panel panel-default">
+                          <div className="panel-heading">
+                          <h3 className="panel-title" >{bill.bill_id}</h3>
+                          </div>
+                          <p className="panel-body">{bill.title}</p>
+                        <button className=".btn-info" ><a href="http://api.fdsys.gov/link?collection=bills&billtype={bill.bill_type}&billversion=ih&billnum={bill.number}&congress=115" target="_blank">View Bill</a></button>
+                        </div>);
+               
+              })
               }
-            </ol>
           </div>
          
         </div>
