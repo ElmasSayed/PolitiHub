8a804c0f01cf0af4890cf74a729f0b7e94626fbb
joe <joemulick@gmail.com>
2017-06-22 11:29:03 -0700

    Updating master with working version

--- app/components/Grandchild.js
+++ app/components/Grandchild.js
@@ -8,7 +8,7 @@ getInitialState: function(){
   return { text : this.props.text };
 },
   render: function() {
-    console.log(this.props.data);
+
     return (
     <div className="row grandchild">
     <div className="col-sm-12 display-box">
@@ -25,19 +25,23 @@ getInitialState: function(){
           */}
           <div className="grandchild-title-div">
           {this.props.text}
-          {
-                
-               this.props.data.map(function(bill, index){
-               return (<div key={index} className="panel panel-default">
-                          <div className="panel-heading">
-                          <h3 className="panel-title" >{bill.bill_id}</h3>
-                          </div>
-                          <p className="panel-body">{bill.title}</p>
-                        <button className=".btn-info" ><a href="http://api.fdsys.gov/link?collection=bills&billtype={bill.bill_type}&billversion=ih&billnum={bill.number}&congress=115" target="_blank">View Bill</a></button>
-                        </div>);
-               
-              })
+              <ol>
+              {
+                this.props.data.map((arg) => {
+                  if (this.props.categories == "House" || "Senate"){
+                   return (
+                      <li className="grandchild-title">
+                      <a href={arg.govtrack_url} target="_blank">{arg.title}</a>
+                      </li>);
+                  } else {
+                      return (
+                      <li className="grandchild-title">
+                      <a href="https://openstates.org/ca/bills/{arg.session}/{arg.bill_id}/" target="_blank">{arg.title}</a>
+                      </li>);
+                  }
+                })
               }
+            </ol>
           </div>
          
         </div>
--- app/components/Parent.js
+++ app/components/Parent.js
@@ -1,47 +1,66 @@
 // Include React
 var React = require("react");
 var Axios = require("axios");
-
-
-//var helpers = require("./utils/helper");
+var helpers = require("./utils/helper");
 
 // Here we include all of the sub-components
 var Child = require("./Child");
 
-// the parent is the overall dashboard but it only actually displays the jumbrotron
 var Parent = React.createClass({
 
-  // Here we set a generic state associated with the number of clicks
+
   getInitialState: function() {
     return {
-      categories: '',
+      categories: 'State',
       data: []
     };
   },
 
-  componentDidMount: function() {
-    // var parameter = 'introduced';
-    // var key = '&apikey=yQGAnrKoJi7WWVfZ4LsbCINgYlhrXrm9YrpF2zE8';
-    // var url = 'https://api.propublica.org/congress/v1/115/house/bills/${parameter}.json${key}';
-    
-   Axios.get('https://openstates.org/api/v1/bills/?state=ca&q=all&apikey=cd8b051a-7c89-4c20-9d63-cbabf9ab8ebf', {}).then(function(data){
-    console.log(data);
-    this.setState({data: data.data}) 
-   }.bind(this));
-   },
-  // Whenever the button is clicked we'll use setState to add to the clickCounter
-  // Note the syntax for setting the state
-  handleClick: function() {
-    this.setState({ categories: this.state.categories});
-  },
-  // Whenever the button is clicked we'll use setState to reset the clickCounter
-  // This will reset the categories -- and it will be passed  ALL children
-  resetClick: function() {
-    this.setState({ categories: ''});
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
   },
 
-  // Here we render the function
+  handleClick: function(event) {
+
+    event.preventDefault();
+
+    this.setState({ categories: event.target.getAttribute('data-value')});
+
+    var current = event.target.getAttribute('data-value');
+
+    if(current == "House"){
+      helpers.runHouse().then(function(billData){
+      this.setState({ data : billData});
+    }.bind(this));
+    
+    }
+    if(current == "Senate"){
+      helpers.runSenate().then(function(billData){
+      this.setState({ data : billData});
+    }.bind(this));
+       
+
+    }if(current == "State"){
+        helpers.runState().then(function(billData){
+      console.log(billData);
+      this.setState({ data : billData.data});
+      console.log ( billData.data);
+    }.bind(this));
+      
+    }
+    if(current == "Local"){
+      helpers.runLocal().then(function(billData){
+      console.log(billData);
+      this.setState({ data : billData.data});
+      console.log ( billData.data);
+    }.bind(this));
+
+    }
+
+  },
+
   render: function() {
+    console.log(this.state.categories, "state");
     return (
       <div className="container-fluid parent">
        
@@ -50,34 +69,23 @@ var Parent = React.createClass({
                 <div className="elmasAA">
                     <div className="col-md-6">
                         <p>
-                            {/* Here we create a button click. Note how we have an onClick event associate with our handleClick function.
-                            <button className="btn btn-primary btn-lg" onClick={this.handleClick}>CLICK ME!!!!</button>
-                            <button className="btn btn-danger btn-lg" onClick={this.resetClick}>Reset</button>
-                            */}
+
                         </p>
                     </div>
                 </div>
                 <div className="col-md-12">
-                                {/* This is where we'll show the click count for the parent **<h1>{this.state.categories}</h1>** */}
-
-                        {/*
-                      Here we'll render the Child component.
-                      We'll pass it this component's clicks counter (this.state.clicks)
-                      as a "prop" we will also name "clicks".
-                      i.e.
-                      <ComponentName propName={propValue} />
-                    */}
+           
                     <div id="tabs" className="btn btn-group-justified">
-                        <a className="btn categories" onClick={this.handleClick}>House</a>
-                        <a className="btn categories" onClick={this.handleClick}>Senate</a>
-                        <a className="btn categories" onClick={this.handleClick}>State</a>
-                        <a className="btn categories" onClick={this.handleClick}>Local</a>
+                        <a className="btn categories" data-value="House" onClick={this.handleClick}>House</a>
+                        <a className="btn categories" data-value="Senate" onClick={this.handleClick}>Senate</a>
+                        <a className="btn categories" data-value="State" onClick={this.handleClick}>State</a>
+                        <a className="btn categories" data-value="Local" onClick={this.handleClick}>Local</a>
                     </div>
                 </div>
             </div>
         </div>
 
-        <Child data={this.state.data} />
+        <Child data={this.state.data} categories={this.state.categories} />
 
       </div>
 
