acade9844982ce68a80e4932aa2571f4853878cc
joe <joemulick@gmail.com>
2017-06-22 10:23:19 -0700

    react components loading all tabs correctly except local. filters not filtering yet.

--- app/components/Child.js
+++ app/components/Child.js
@@ -1,16 +1,13 @@
-// Include React
 var React = require("react");
-
-// Here we include all of the sub-components
 var GrandChild = require("./GrandChild");
 
 // the child is the componenent that takes up all the space under the jumbotron in parent but only displays actual content on the left third of the page
 var Child = React.createClass({
+
   render: function() {
     return (
     <div className="row child main-area">
     <div className="col-sm-3 filter-box">
-
       <div className="panel panel-default ">
       
         <div className="panel-heading">
@@ -19,21 +16,12 @@ var Child = React.createClass({
         <div className="panel-body text-center">
               <br></br>
               <br></br>
-          {/*
-            This component recieves a clicks variable as a prop from it's parent.
-            <h1>{this.props.clicks}</h1>
-          */}
-
-          {/*
-            This component will then render the GrandChild component. It will give GrandChild a prop
-            called "number" which will be calculated by mutiplying this.props.clicks by 2
-          */}
           <h3> here we insert all of the work elmas did on the left hand column with all the sorting info </h3>
         </div>
       </div>
       </div>
          <div className="col-sm-9">
-          <GrandChild data={this.props.data} category={this.props.categories} />
+          <GrandChild data={this.props.data} categories={this.props.categories} />
          </div>
      </div>
    
@@ -41,5 +29,5 @@ var Child = React.createClass({
   }
 });
 
-// Export the component back for use in other files
+
 module.exports = Child;
--- app/components/Grandchild.js
+++ app/components/Grandchild.js
@@ -1,43 +1,40 @@
 // Include React
 var React = require("react");
 
-// the grandchild is the actual posting of all the articles
 var GrandChild = React.createClass({
- 
-var = currentLink
 
+ 
   render: function() {
-    console.log(this.props.data);
+
     return (
     <div className="row grandchild">
     <div className="col-sm-12 display-box">
       
       <div className="panel panel-default ">
         <div className="panel-heading">
-          <h3 className="panel-title">this is where the legislation is posted</h3>
+          <h3 className="panel-title text-center">Most Recent Bills</h3>
         </div>
         <div className="panel-body">
         <h3> HB 2017 </h3>
         <h5> here we have a description of what the legislation will be, taken from the 'title' of the api </h5>
-          {/*
-            This component will display 4 times what it's parent (Child) displays
-          */}
+
           <div className="grandchild-title-div">
             <ol>
-            {
-              this.props.data.map(function(arg){
-              if (categories=== "house" || "senate"){
-               return (<li className="grandchild-title">
-                        <a href="whatever hyperlink here" target="_blank">{arg.title}</a>
-                        </li>);
-              } else {
-                        return (<li className="grandchild-title">
-                        <a href="whatever hyperlink here" target="_blank">{arg.title}</a>
-                        </li>);
+              {
+                this.props.data.map((arg) => {
+                  if (this.props.categories == "House" || "Senate"){
+                   return (
+                      <li className="grandchild-title">
+                      <a href={arg.govtrack_url} target="_blank">{arg.title}</a>
+                      </li>);
+                  } else {
+                      return (
+                      <li className="grandchild-title">
+                      <a href="https://openstates.org/ca/bills/{arg.session}/{arg.bill_id}/" target="_blank">{arg.title}</a>
+                      </li>);
+                  }
+                })
               }
-
-              })
-            }
             </ol>
           </div>
          
